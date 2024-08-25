4fe47610f8214b0fdf6afd40aac88750880716e9
PenguinSquire <syates03@gmail.com>
2024-08-24 21:23:15 -0400

    added 'universal' commands folder
    folder where commands are allowed to be used anywhere, as opposed to "global" commands which can only be used in servers the bot is in

--- global-deploy.js
+++ global-deploy.js
@@ -10,7 +10,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	// Grab all the command files from the commands directory you created earlier
-	if (folder == "global") {
+	if (folder == "global" || folder == "universal") {
 		const commandsPath = path.join(foldersPath, folder);
 		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
@@ -19,9 +19,11 @@ for (const folder of commandFolders) {
 			const command = require(filePath);
 			if ('data' in command && 'execute' in command) {
 				// allows commands to also be used in DM's with other people; not sure which staement actually allows it tho :/
-				command.data.contexts = [0, 1, 2];
-				command.data.integration_types = [0, 1];
-				
+				if (folder == "universal") {
+					command.data.contexts = [0, 1, 2];
+					command.data.integration_types = [0, 1];
+				}
+
 				commands.push(command.data.toJSON());
 			} else {
 				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
