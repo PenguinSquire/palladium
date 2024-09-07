d549700dabb021447eb39415382d5ecdb4cc8435
PenguinSquire <syates03@gmail.com>
2024-09-07 02:40:22 -0400

    log now records date

--- modules/log.js
+++ modules/log.js
@@ -15,10 +15,14 @@ function sendLog (commandID, message, label) {
     logToFile(`${label} ${commandID} - ${message}`)
     console.log(`${commandID} - `, message)
 }
-
+const d = new Date();
+let day =  d.getDay() + 1
+let  month = d.getMonth() + 1
+let paddedDay = ("00" + day).slice(-2);
+let paddedMonth = ("00" + month).slice(-2);
 module.exports = {
     info: (commandID, message) => sendLog (commandID, message, ` [INFO]`),
     warn: (commandID, message) => sendLog (commandID, message, ` [WARN]`),
     error: (commandID, message, err) => sendLog(commandID, (message, err), `[ERROR]`),
-    link: (commandID, message) => sendLog (commandID, message, `\n       `)
+    link: (commandID, message) => sendLog (commandID, message, `\n${paddedDay}/${paddedMonth}: `)
 }
