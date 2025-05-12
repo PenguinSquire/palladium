1aa9d947d136be5392b02d514d2a5db87be1b0aa
PenguinSquire <syates03@gmail.com>
2025-05-12 14:45:31 -0400

    COBALT IS BACK
    requires a locally run version of Cobalt but STILL WE'RE SO BACK

--- modules/backupEmbed.js
+++ modules/backupEmbed.js
@@ -7,23 +7,26 @@ function backupEmbed(link) {
     let protocol = url.protocol
     let host = url.host
     let pathname = url.pathname;
-    console.log(host)
+    let search = url.search;
+    //console.log(url)
     if ((/^www\./i.test(url.host))) {
         www = 'www.'
         host = host.slice(4, host.length)
     }
     if (host.includes("instagram")) {
         fix = 'dd'
+        search = ''
     } else if (host.includes("tiktok")) {
         host = host.slice(2, host.length)
         fix = 'tfx'
+        search = ''
     } else if (host.includes("twitter")) {
         fix = 'vx'
     } else if (host.includes("x.com")) {
         host = host.slice(1, host.length)
         fix = 'vxtwitter'
     }
-    let newLink = protocol + '//' + www + fix + host + pathname
+    let newLink = protocol + '//' + www + fix + host + pathname + search
     return newLink
 }
 module.exports = { backupEmbed }
